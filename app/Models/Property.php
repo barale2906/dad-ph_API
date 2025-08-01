ee5de5a9f1e2bb39771d1e58abe1038930707e10
barale2906 <alexanderbarajas@gmail.com>
2025-08-01 16:28:41 -0500

    Permiso de registro masivo en los campos de los modelos

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -10,6 +10,12 @@ class Property extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'tower_block',
+        'unit_number',
+        'ownership_coefficient',
+    ];
+
     public function users(): BelongsToMany
     {
         return $this->belongsToMany(User::class)->withPivot('relationship_type')->withTimestamps();
